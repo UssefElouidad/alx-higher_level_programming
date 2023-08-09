68fcb73214158f6833d54c36611348f6e2ff7901
youssef el ouidad <y.elouidad@gmail.com>
2023-08-10 00:08:17 +0100

    8. Create a new sentence

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-str = " ".join(str.split(", ")[2].split()[:2]) + " " + str.split()[-4] +\
-        " " + str.split()[0]
+str = " ".join(str.split(", ")[2].split()[:2]) + " " + str.split()[-4] + " " + str.split()[0]
 print(str)
