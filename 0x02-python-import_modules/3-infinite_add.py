c24b343476038bb4fbc93e81aeb2af8f7b18c016
youssef el ouidad <y.elouidad@gmail.com>
2023-08-15 00:14:38 +0100

    Infinite addition

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-import sys
-n = len(sys.argv) - 1
-total = sum(int(sys.argv[x]) for x in range(1, n + 1))
-print("{:d}".format(total))
+if __name__ == "__main__":
+    import sys
+    n = len(sys.argv) - 1
+    total = sum(int(sys.argv[x]) for x in range(1, n + 1))
+    print("{:d}".format(total))
