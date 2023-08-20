86ed063ea6e4ba9949f8aa74a6088323f564fe43
youssef el ouidad <y.elouidad@gmail.com>
2023-08-20 21:31:16 +0100

    smile in the miror

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
 for a in range (ord('Z'), ord('A') - 1, -1):
+    output = ""
     char = chr(a)
     print(f"{char.lower() if a % 2 == 0 else char.upper()}", end="")
+    output += "{}".format(char.lower()) if a % 2 == 0 else "{}".format(char.upper())
+
+print("{}".format(output), end="")
+  
