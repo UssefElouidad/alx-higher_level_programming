b5e00af0be682d2b50541e6ddae976998cb32dee
youssef el ouidad <y.elouidad@gmail.com>
2023-08-20 22:13:44 +0100

    smile in the miror

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-for a in range (ord('Z'), ord('A')  - 1, -1):
-    if ((a +1) % 2 == 1):
+for a in range(ord('Z'), ord('A') - 1, -1):
+    if ((a + 1) % 2 == 1):
         a += 32
     print("{:c}".format(a), end="")
-  
