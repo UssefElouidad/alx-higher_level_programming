c4f46df733174b35637f014bd2d4811afd1106a9
youssef el ouidad <y.elouidad@gmail.com>
2023-08-29 23:13:37 +0100

    my first class

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
 class Square:
-    """ This is a simple class that define a square"""
-    pass
+    """ This is a simple class that define a square
+    Attributes:
+        None
+    """
+    def __init__(self):
+        """
+        Constructor for Square class.
+        Args:
+            None
+        """
+            pass
