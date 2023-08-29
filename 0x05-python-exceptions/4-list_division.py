1610a6f7fd4ef598a6568c2d7e96c07943ee8d64
youssef el ouidad <y.elouidad@gmail.com>
2023-08-29 01:16:54 +0100

    raise exception

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
+    count = 0
+    for i, j in zip(my_list_1, my_list_2):
+        new_list[z] = my_list_1[i] / my_list_2[j]
+        count += 1
     
