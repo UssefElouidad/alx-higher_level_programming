ebf6e5328f83ebedc0adf200470d037476d4bda7
youssef el ouidad <y.elouidad@gmail.com>
2023-09-12 00:57:42 +0100

    improve geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" defines an empty class"""
+
+
+class BaseGeometry:
+    """ an empty class
+    """
+    def area(self):
+        raise TypeError("area() is not implemented")
