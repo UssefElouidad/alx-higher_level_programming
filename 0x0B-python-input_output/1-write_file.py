fdf759634988b8ca28583d468bdc1cedae1bfb00
youssef el ouidad <y.elouidad@gmail.com>
2023-09-13 00:29:39 +0100

    write to file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" defines a file function """
+
+
+def write_file(filename="", text=""):
+    """ a function that writes a string to a text file
+    and returns the number of carachters written
+    """
+    try:
+        with open(filename, "w", encoding="utf-8") as file:
+            chars_written = file.write(text)
+        return chars_written
+    except:
+        pass
+
