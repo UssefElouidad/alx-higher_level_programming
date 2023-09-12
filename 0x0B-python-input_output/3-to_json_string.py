e4b1c175cce2d6dfdae37baa552992f997d373ed
youssef el ouidad <y.elouidad@gmail.com>
2023-09-13 00:57:53 +0100

    To JSON string

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """ defines a file function"""
 
-import json 
+import json
+
+
 def to_json_string(my_obj):
     """ a function that returns the JSON representation of an object
     """
