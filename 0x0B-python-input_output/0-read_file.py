318cd9fe9ca4915f3abc7a83b8f624406d0bc090
youssef el ouidad <y.elouidad@gmail.com>
2023-09-13 00:01:45 +0100

    read file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" defines a file function """
+
+
+def read_file(filename=""):
+    """ a function that reads a text file
+    and prints it to stdout
+    """
+        with open(filename, encoding='utf-8') as f:
+            print(f.read())
