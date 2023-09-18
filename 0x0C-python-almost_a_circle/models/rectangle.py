c80e9050664aaff4a6e1e64fa8cbe055413a7e7a
youssef el ouidad <y.elouidad@gmail.com>
2023-09-18 03:27:18 +0100

    __str__

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -62,3 +62,5 @@ class Rectangle(Base):
             for _ in range(self.width):
                 print('#', end="")
             print()
+    def __str__(self):
+        return f"[Rectangle] ({self.id}) {self.x}/{self.y} - {self.width}/{self.height}"
