ccf6af8a0de79654ecbc576f844f6fb12ddb70b6
youssef el ouidad <y.elouidad@gmail.com>
2023-09-22 20:35:19 +0100

    Dictionary to JSON string

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -21,6 +21,6 @@ class Base:
     def to_json_string(list_dictionaries):
         """ the method that converts the list
         of dictionaries to a JSON string"""
-        if list_dictionaries is None or len(list_dictionaries == 0):
+        if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
-        return json.dumps(lsit_dictionaries)
+        return json.dumps(list_dictionaries)
