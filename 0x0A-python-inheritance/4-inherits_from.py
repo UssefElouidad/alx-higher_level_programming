e7f26c734896d99b319d8fff8d41efb8744cd427
youssef el ouidad <y.elouidad@gmail.com>
2023-11-06 23:17:32 +0100

    task 3 to 5

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" defines a class checker function """
+
+
+def inherits_from(obj, a_class):
+    """ a function that returns True
+    if the object is an instance of a class
+    that inherited (directly or indirectly)
+    from the specified class ; otherwise False.
+    """
+    return issubclass(type(obj), a_class) and type(obj) is not a_class
