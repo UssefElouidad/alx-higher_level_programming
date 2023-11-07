ef308d71663cbeb0738b72b7efa37b9b30452fc2
youssef el ouidad <y.elouidad@gmail.com>
2023-11-07 23:37:59 +0100

    task 5

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" defines a text function """
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """ a function that writes an object to a text file
+    using a JSON represntation
+    """
+    with open(filename, "w", encoding="utf-8") as f:
+        json.dump(my_obj, f)
