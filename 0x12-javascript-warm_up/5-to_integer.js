435c9155889cfd96ebd4e86a33838674448682b6
youssef el ouidad <y.elouidad@gmail.com>
2024-01-09 00:45:53 +0100

    5. an integer

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const args = process.argv.slice(2);
+
+if (args === undefined) {
+  console.log('Not a number');
+} else {
+  const intValue = parseInt(args);
+  if (isNaN(intValue)) {
+    console.log('Not a number');
+  } else {
+    console.log(intValue);
+  }
+}
