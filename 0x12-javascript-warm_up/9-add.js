ea1879c68a1f60f081ebc49edb294ebe58e0516d
youssef el ouidad <y.elouidad@gmail.com>
2024-01-09 01:42:07 +0100

    9. add

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const args = process.argv.slice(2);
+if (args.length = 2) {
+  function add(a, b) {
+    return a + b;
+  }
+  const result = add(parseInt(args[0]), parseInt(args[1]));
+    console.log(result);
+}
+
