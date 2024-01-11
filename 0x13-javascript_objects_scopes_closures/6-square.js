31c153a7bd0f78b93053d0014a5bc3118e03468c
youssef el ouidad <y.elouidad@gmail.com>
2024-01-11 01:03:41 +0100

    tasks 4,5 and 6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -6,12 +6,12 @@ class Square1 extends Square {
   costructor (size) {
   }
 
-  charPrint(c) {
+  charPrint (c) {
     if (c === undefined) {
       c = 'X';
     }
     for (let i = 0; i < this.height; i++) {
-	    console.log(c.repeat(this.width));
+      console.log(c.repeat(this.width));
     }
   }
 }
