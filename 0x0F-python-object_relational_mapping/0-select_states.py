3ed897b517290922c908f951c76807af826c7cfd
youssef el ouidad <y.elouidad@gmail.com>
2024-01-14 23:15:58 +0100

    task 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,9 +1,33 @@
 #!/usr/bin/python3
+"""
+0-select_states.py - A script to connect to a MySQL server, retrieve and display states from the hbtn_0e_0_usa database.
+Usage:
+    python 0-select_states.py <username> <password> <database>
+Arguments:
+    username (str): MySQL username.
+    password (str): MySQL password.
+    database (str): MySQL database name.
+
+Example:
+    python 0-select_states.py root password hbtn_0e_0_usa
+"""
+
 import MySQLdb
 import sys
 
 def list_states(username, password, database):
-    # Connect to MySQL server
+    """
+    Connects to a MySQL server and retrieves states from the 'states' table.
+
+    Args:
+        username (str): MySQL username.
+        password (str): MySQL password.
+        database (str): MySQL database name.
+
+    Returns:
+        None
+    """
+    
     db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
     cursor = db.cursor()
 
