5251ed9eecc9d42733d3fe783c3df189fac49917
youssef el ouidad <y.elouidad@gmail.com>
2024-03-02 19:21:44 +0100

    b

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,14 +1,6 @@
 -- a script that creates a table called first_table in the current database
-
--- Create the database if it doesn't exist
-CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
-
--- Use the database
-USE hbtn_0c_0;
-
--- Create the table if it doesn't exist
+CREATE DATEBASE IF NO EXISTS hbtn_0c_0;
 CREATE TABLE IF NOT EXISTS first_table (
   id INT,
   name VARCHAR(256)
 );
-
--- 0x12-javascript-warm_up/4-concat.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/node
-
-const args = process.argv.slice(2);
-
-if (!args[2]) {
-  console.log(args[0] + ' is ' + args[1]);
-}
