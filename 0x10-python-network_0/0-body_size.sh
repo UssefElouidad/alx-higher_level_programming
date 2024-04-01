04c5af1879af97e6b6cfc71ff1e0c85980e27739
youssef el ouidad <y.elouidad@gmail.com>
2024-04-01 17:47:00 +0000

    task 0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -2,4 +2,11 @@
 
 # a bash script that takes in a URL and displays the size of the body of the response
 
-curl -sI "$1" | wc -c
+# Get URL from command line argument
+URL=$1
+
+# Send request to the URL and store response body in a temporary file
+response=$(curl -sI $URL | grep -i "Content-Length" | awk '{print $2}')
+
+# Display the size of the body in bytes
+echo $response
